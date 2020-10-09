bf8a22f5cebfee80bb4ab24da62682a6446b3102
melondulzon <melondulzon@gmail.com>
2020-10-09 18:25:34 +0200

    1st commit

--- lib/notion/server-constants.ts
+++ lib/notion/server-constants.ts
@@ -1,2 +1,2 @@
-export const PAGE_ID = '1a86e7f6-d6a5-4537-a2e5-15650c1888b8'
+export const PAGE_ID = '602f8197-1bc9-42f3-9ea0-248f4d53efef' // De la url agafem l'últim tros i afegim guions cada 7-4-4-4-N
 export const API_ENDPOINT = 'https://www.notion.so/api/v3'
